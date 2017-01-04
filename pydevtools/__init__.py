8d4f0e9b9120921e40c995c9a3d32bd28e2128b8
Alan Bridger <alan@kano.me>
2017-01-04 17:54:23 +0000

    Add header to __init__ file

---
+++ pydevtools/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""pydevtools, a collection of Python-related dev tools
+"""
+
+__copyright__ = "Copyright 2017, Kano Computing Ltd."
+__license__ = "GPLv2"
+__version__ = "1.0.1"
+__maintainer__ = "Tom Bettany"
+__email__ = "tom@kano.me"
