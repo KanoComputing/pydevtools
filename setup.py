dbd3f04bd899e7a213e026de61d4f44784423a2c
Radek Pazdera <radek@kano.me>
2017-01-04 18:01:35 +0000

    Adding basic setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pydevtools',
+    version='1.0',
+    description='An assortment of python devtools',
+    author='Kano Computing Limited',
+    author_email='dev@kano.me',
+    maintainer='Tom Bettany',
+    maintainer_email='tom@kano.me',
+    url='https://github.com/KanoComputing/pydevtools',
+    packages=['pydevtools'],
+    license='GPLv2'
+    )
