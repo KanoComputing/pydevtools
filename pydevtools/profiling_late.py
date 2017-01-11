c67c5951e3f2b145c956e34fb42c5fcb691f4397
Albert Casals <skarbat@gmail.com>
2017-01-11 18:24:44 +0000

    Style fixes, pyhon 2 and 3 compatibility

--- pydevtools/profiling_late.py
+++ pydevtools/profiling_late.py
@@ -10,19 +10,21 @@ Module to enable profiling timepoints. This module is loaded
 only if the configuration file exists, see profiling.py for more information
 '''
 
+from __future__ import print_function
+
 import os
 import sys
 import yaml
 import cProfile
 
-from profiling import CONF_FILE, defaultConf
+from profiling import CONF_FILE, DEFAULT_CONF
 
 # load the configuration file or set default builtin settings
 try:
     with open(CONF_FILE, 'r') as inp_conf:
         conf = yaml.load(inp_conf)
 except:
-    conf = yaml.safe_dump(defaultConf)
+    conf = yaml.safe_dump(DEFAULT_CONF)
 
 myProfile = cProfile.Profile()
 app_name = sys.argv[0]
@@ -50,14 +52,14 @@ def declare_timepoint(name, isStart):
                 pythonProfile = True
                 if isStart:
                     if point_current:
-                        print 'Stop profiling for point "{0}" and do "{1}" instead'.format(point_current, name)
+                        print('Stop profiling for point "{0}" and do "{1}" instead'.format(point_current, name))
                         myProfile.disable()
                         myProfile.clear()
                     point_current = name
                     myProfile.enable()
                 else:
                     if point_current != name:
-                        print 'Can\'t stop point "{0}" since a profiling session for "{1}" is being run'.format(name, point_current)
+                        print('Can\'t stop point "{0}" since a profiling session for "{1}" is being run'.format(name, point_current))
                     else:
                         myProfile.disable()
                         # Check if the statfile location in specified
@@ -66,15 +68,15 @@ def declare_timepoint(name, isStart):
                                 myProfile.dump_stats(ct['python']['statfile'])
                             except IOError as e:
                                 if e.errno == 2:
-                                    print 'Path to "{}" probably does not exist'.format(ct['python']['statfile'])
+                                    print('Path to "{}" probably does not exist'.format(ct['python']['statfile']))
                                 else:
-                                    print 'dump_stats IOError: errno:{0}: {1} '.format(e.errno, e.strerror)
+                                    print('dump_stats IOError: errno:{0}: {1} '.format(e.errno, e.strerror))
                         else:
-                            print 'No statfile entry in profiling conf file "{}"'.format(CONF_FILE)
+                            print('No statfile entry in profiling conf file "{}"'.format(CONF_FILE))
                         myProfile.clear()
                         point_current = ""
             else:
-                print 'Profiling conf file doesnt enable the Python profiler for point {} at app {}'.format(name, app_name)
+                print('Profiling conf file doesnt enable the Python profiler for point {} at app {}'.format(name, app_name))
 
             # Check if we want to run some other command at this timepoint
             if isStart and has_key(ct, 'start_exec'):
@@ -84,8 +86,8 @@ def declare_timepoint(name, isStart):
                 cmd = ct['end_exec']
                 os.system(cmd)
         else:
-            print 'Profiling conf file doesnt include point:{} for app {}'.format(name, app_name)
+            print('Profiling conf file doesnt include point:{} for app {}'.format(name, app_name))
     else:
-        print 'Profiling conf file doesnt include app:{}'.format(app_name)
+        print('Profiling conf file doesnt include app:{}'.format(app_name))
 
-    print 'timepoint ', name, name, isStart, cmd, pythonProfile
+    print('timepoint ', name, name, isStart, cmd, pythonProfile)
