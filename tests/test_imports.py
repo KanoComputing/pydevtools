e2fb0fbf715e3982b4f406c15c09698c50f50f52
Albert Casals <skarbat@gmail.com>
2017-01-11 17:47:36 +0000

    fixing tests path

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -2,7 +2,10 @@
 # Simple tests to see that pytest works
 #
 
-def test_load_():
+import sys
+sys.path.append('..')
+
+def test_import_modules():
     import pydevtools.profiling
     import pydevtools.profiling_late
     import pydevtools.pydebug
